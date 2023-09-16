1716543845047f6401dac561140b2be23c7cb295
Sergio Calahorra <sergiocalahorra20@gmail.com>
2023-09-16 18:45:28 +0200

    Install firebase

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,24 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { environment } from "./environments/environment";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: environment.firebaseConfig.apiKey,
+  authDomain: environment.firebaseConfig.authDomain,
+  projectId: environment.firebaseConfig.projectId,
+  storageBucket: environment.firebaseConfig.storageBucket,
+  messagingSenderId: environment.firebaseConfig.messagingSenderId,
+  appId: environment.firebaseConfig.appId,
+  measurementId: environment.firebaseConfig.measurementId
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 if (environment.production) {
   enableProdMode();
