bc4a1f6319d4d98783b4dc4312e2c77dbdf341c0
Sergio Calahorra <sergiocalahorra20@gmail.com>
2023-09-17 17:04:43 +0200

    Creation of alert for failures in login

--- src/app/components/auth-modal/auth-modal.component.ts
+++ src/app/components/auth-modal/auth-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-auth-modal',
@@ -8,9 +9,46 @@ import { ModalController } from '@ionic/angular';
 })
 export class AuthModalComponent  implements OnInit {
 
-  constructor( private modalController: ModalController ) { }
+  constructor(
+    private modalController: ModalController,
+    private authService: AuthService,
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {}
+
+  @HostListener('document:click', ['$event'])
+  handleDocumentClick(event: Event) {
+    const modalElement = document.querySelector('.modal-container');
+
+    if (modalElement && !modalElement.contains(event.target as Node)) {
+      this.modalController.dismiss();
+    }
+  }
+
+  signinWithGoogle() {
+    this.authService.loginWithGoogle()
+      .then(response => {
+        this.authService.mapUserInfo(response);
+        console.log('Sesión iniciada correctamente con Google', response);
+      })
+      .catch(async error => {
+        const alert = await this.alertController.create({
+          animated: true,
+          buttons: ['OK'],
+          header: 'Error',
+          message: 'No se ha podido iniciar sesión',
+          mode: 'ios'
+        });
+        await alert.present();
+        console.log('Error al iniciar sesión con Google', error);
+      });
+    this.modalController.dismiss();
+  }
+
+  async signinWithEmail() {
+    
+  }
 
   close() {
     this.modalController.dismiss();
